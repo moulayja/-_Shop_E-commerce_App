ec3c0e4620a77b50b5b356c9bf23524d8f2a383c
Jamal  Farah <moulayjam@gmail.com>
2020-05-21 10:59:02 -0400

    Update SignIn.js

--- front_end/src/components/Authentication/SignIn.js
+++ front_end/src/components/Authentication/SignIn.js
@@ -20,7 +20,7 @@ export default class SignIn extends Component {
         event.preventDefault();
         console.log(this.state);
 
-        fetch(`http://13.58.204.144:3000/users/login`,{
+        fetch(`http://13.58.204.144:3001/users/login`,{
             method: 'POST',
             headers: {
                 "Authorization": "token_go",
